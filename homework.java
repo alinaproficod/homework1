71446f9da205b7162caab7053fe32912127aa37c
Alina <131201532+alinaproficod@users.noreply.github.com>
2023-05-01 09:57:29 +0200

    java2
    java/

---
+++ homework.java
@@ -0,0 +1,15 @@
+package lesson1;
+
+public class homework {
+    public static void main(String[] args){
+
+        System.out.println("char" + "G");
+        System.out.println("inteiger" + "89");
+        System.out.println("byte" + "4");
+        System.out.println("short" + "56");
+        System.out.println("float" + "4.7333436");
+        System.out.println("double" + "4.355453532");
+        System.out.println("long" + "12121");
+
+    }
+}
