d921e98a8232d5fe74960ada979e220eedd7c437
Alina <131201532+alinaproficod@users.noreply.github.com>
2023-05-07 17:45:09 +0200

    Update class23.java

--- class23.java
+++ class23.java
@@ -7,7 +7,7 @@ public class class23 {
         System.out.println(str.length());
         System.out.println(str.charAt(14));
         System.out.println(str.contains("Java"));
-        System.out.println(str.substring(11 , 14));
+        System.out.println(str.substring(14 , 18));
         System.out.println(str.toUpperCase());
         System.out.println(str.toLowerCase());
         System.out.println(str.replace("a" , "o"));
