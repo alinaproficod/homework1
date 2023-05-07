5adc096ef48bac45e6058dc9d48895a435941cc5
Alina <131201532+alinaproficod@users.noreply.github.com>
2023-05-07 14:26:15 +0200

    Add files via upload

---
+++ elf.java
@@ -0,0 +1,40 @@
+package game.hero;
+
+public class elf {
+    private int id;
+    private String name;
+    private double d;
+    private double price;
+    private long l;
+    private char ch;
+    public static void main(String[] args) {
+
+        int getIng;
+        char setId;
+        String getName;
+        long gettId;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
