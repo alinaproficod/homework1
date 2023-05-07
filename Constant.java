08cae129b46dcfb42c91f24878ff76fec41e3266
Alina <131201532+alinaproficod@users.noreply.github.com>
2023-05-07 14:24:37 +0200

    Add files via upload

---
+++ Constant.java
@@ -0,0 +1,14 @@
+package game.hero;
+
+public class Constant {
+    public static void main(String[] args) {
+
+         final  int ELF_POWER = 10;
+         int ELF_CHARISMA = 8;
+         int ELF_MAGIC = 12;
+        System.out.println("int" + ELF_POWER);
+        System.out.println("int" + ELF_CHARISMA);
+        System.out.println("int" + ELF_MAGIC);
+
+    }
+}
