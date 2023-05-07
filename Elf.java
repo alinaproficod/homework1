ba34217aa7a28ccd8a6c6c2fb5f609f27f82cf69
Alina <131201532+alinaproficod@users.noreply.github.com>
2023-05-07 21:24:05 +0200

    Add files via upload

---
+++ Elf.java
@@ -0,0 +1,66 @@
+
+package game.hero;
+
+public class Elf {
+    private int id;
+
+    private int weight;
+    private int height;
+    private int age;
+
+
+    public int getId() {
+        return id;
+
+        }
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+
+
+        public void setHeight(int height) {
+            this.height = height;
+        }
+
+        public int getAge() {
+
+            return age;
+        }
+        public void setAge (int age) {
+
+            this.age = age;
+        }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
