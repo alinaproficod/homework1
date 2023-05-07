5a07b5bc1b7bd48c537a362a7bf4fe6a2809b785
Alina <131201532+alinaproficod@users.noreply.github.com>
2023-05-07 17:15:56 +0200

    java3

---
+++ class23.java
@@ -0,0 +1,23 @@
+package lesson3;
+
+public class class23 {
+    public static void main(String[] args) {
+
+        String str = new String("I study Basic Java!");
+        System.out.println(str.length());
+        System.out.println(str.charAt(14));
+        System.out.println(str.contains("Java"));
+        System.out.println(str.substring(11 , 14));
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        System.out.println(str.replace("a" , "o"));
+        
+
+
+
+
+    }
+
+
+    }
+
