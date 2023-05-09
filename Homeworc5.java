4850aec42b5661def8985df534146a399f7bb00c
Alina <131201532+alinaproficod@users.noreply.github.com>
2023-05-09 18:22:22 +0200

    java4

---
+++ Homeworc5.java
@@ -0,0 +1,19 @@
+package lesson1;
+
+import java.sql.SQLOutput;
+
+public class Homeworc5 {
+    public static void main(String[] args) {
+      int euro = 7;
+
+        int exchangeRate = 2;
+        System.out.println(euro * exchangeRate);
+        
+
+
+
+
+
+
+    }
+}
