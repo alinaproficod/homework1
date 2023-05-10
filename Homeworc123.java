7e3b19ccabf876c86644a7509e352e34bea73f5f
Alina <131201532+alinaproficod@users.noreply.github.com>
2023-05-10 22:17:32 +0200

    calories

---
+++ Homeworc123.java
@@ -0,0 +1,18 @@
+package lesson1;
+
+public class Homeworc123 {
+    public static void main(String[] args) {
+        int caloRies = 40;
+        int diameterDifference = 28 - 24;
+        int additionalCalories = caloRies * (int) (diameterDifference / 2.0 );
+
+        System.out.println("При замене пиццы диаметром 24 см на пиццу диаметром 28 см,");
+        System.out.println("будет добавлено примерно " + additionalCalories + " calories.");
+
+    }
+}
+
+
+
+
+
